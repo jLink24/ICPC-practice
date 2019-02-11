ce9daa846e5042081f6cd8110c2f04b38122055e
Unknown <justinsiu10@yahoo.com>
2019-02-11 09:58:54 -0500

    Add templates folder

---
+++ Templates/Template1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+// Solution to [Site name] Problem [#]: [Problem Name]
+// https://someproblemlink
+// Solution by: jLink23
+
+public class Template1 {
+  private static PrintWriter out;
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    out = new PrintWriter(System.out);
+
+    while (in.hasNext()) {
+
+    }
+
+    in.close();
+    out.flush();
+  }
+
+}
