a56d48b3213cf9df84857c0f8e86351fd3a7fff4
Unknown <justinsiu10@yahoo.com>
2019-02-17 14:07:31 -0500

    Add Fast IO Template

---
+++ Templates/FastIOTemplate.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+// Solution to [Site name] Problem [#]: [Problem Name]
+// https://someproblemlink
+// Solution by: jLink23
+
+public class FastIOTemplate {
+  private static PrintWriter out;
+
+  public static void main(String[] args) {
+    out = new PrintWriter(System.out);
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    try {
+      String line = br.readLine();
+      StringTokenizer st = new StringTokenizer(line);
+
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    out.flush();
+  }
+
+}
