7d5209a6e7c216a430148d30e4c1896e2aff2922
Unknown <justinsiu10@yahoo.com>
2019-03-03 17:47:42 -0500

    Update FastIOTemplate

--- Templates/FastIOTemplate.java
+++ Templates/FastIOTemplate.java
@@ -10,18 +10,65 @@ public class FastIOTemplate {
 
   public static void main(String[] args) {
     out = new PrintWriter(System.out);
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    FastScanner in = new FastScanner();
 
-    try {
-      String line = br.readLine();
-      StringTokenizer st = new StringTokenizer(line);
-
-
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+    
 
     out.flush();
   }
+
+  public static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner(Reader in) {
+            br = new BufferedReader(in);
+        }
+
+        public FastScanner() {
+            this(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String readNextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] readIntArray(int n) {
+            int[] a = new int[n];
+            for (int idx = 0; idx < n; idx++) {
+                a[idx] = nextInt();
+            }
+            return a;
+        }
+    }
 
 }
