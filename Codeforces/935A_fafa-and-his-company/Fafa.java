fc52a13e3a065eacb8978ebed99a0a0e45a9dd22
Unknown <justinsiu10@yahoo.com>
2019-03-03 18:26:49 -0500

    Add Codeforces #935A: Fafa and his company

---
+++ Codeforces/935A_fafa-and-his-company/Fafa.java
@@ -0,0 +1,81 @@
+import java.util.*;
+import java.io.*;
+
+// Solution to Codeforces Problem 935A: Fafa and his company
+// https://codeforces.com/problemset/problem/935/A
+// Solution by: jLink23
+
+public class Fafa {
+  private static PrintWriter out;
+
+  public static void main(String[] args) {
+    out = new PrintWriter(System.out);
+    FastScanner in = new FastScanner();
+
+    int n = in.nextInt();
+
+    int count = 0;
+    for (int i = 1; i <= n/2; i++) {
+      if ((n-i) % i == 0) count++;
+    }
+
+    out.println(count);
+
+    out.flush();
+  }
+
+  public static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner(Reader in) {
+            br = new BufferedReader(in);
+        }
+
+        public FastScanner() {
+            this(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String readNextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] readIntArray(int n) {
+            int[] a = new int[n];
+            for (int idx = 0; idx < n; idx++) {
+                a[idx] = nextInt();
+            }
+            return a;
+        }
+    }
+
+}
