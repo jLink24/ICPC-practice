6d986e0bdf3329fee3271caf63e23870fb039357
Unknown <justinsiu10@yahoo.com>
2019-03-03 11:59:04 -0500

    Add Codeforces #1030A: In search of an easy problem

---
+++ Codeforces/1030A_in-search-of-an-easy-problem/EasyProblem.java
@@ -0,0 +1,85 @@
+import java.util.*;
+import java.io.*;
+
+// Solution to Codeforces Problem 1030A: In search of an easy problem
+// https://codeforces.com/problemset/problem/1030/A
+// Solution by: jLink23
+
+public class EasyProblem {
+  private static PrintWriter out;
+
+  public static void main(String[] args) {
+    out = new PrintWriter(System.out);
+    FastScanner in = new FastScanner();
+
+    int n = in.nextInt();
+
+    boolean hard = false;
+    for (int i = 0; i < n; i++) {
+      if (in.nextInt() == 1) hard = true;
+    }
+
+    if (hard) {
+      out.println("HARD");
+    } else {
+      out.println("EASY");
+    }
+
+    out.flush();
+  }
+
+  public static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner(Reader in) {
+            br = new BufferedReader(in);
+        }
+
+        public FastScanner() {
+            this(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String readNextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] readIntArray(int n) {
+            int[] a = new int[n];
+            for (int idx = 0; idx < n; idx++) {
+                a[idx] = nextInt();
+            }
+            return a;
+        }
+    }
+
+}
