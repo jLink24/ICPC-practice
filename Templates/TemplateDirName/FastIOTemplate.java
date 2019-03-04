4d58da99d1b0747c573801c3896c040370511dfc
Unknown <justinsiu10@yahoo.com>
2019-03-04 13:13:17 -0500

    Update templates

---
+++ Templates/TemplateDirName/FastIOTemplate.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.io.*;
+
+// Solution to [Site name] Problem [#]: [Problem Name]
+// https://someproblemlink
+// Solution by: jLink23
+
+public class FastIOTemplate {
+  private static PrintWriter out;
+
+  public static void main(String[] args) {
+    out = new PrintWriter(System.out);
+    FastScanner in = new FastScanner();
+
+    
+
+    out.flush();
+  }
+
+  public static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner(Reader in) {
+            br = new BufferedReader(in);
+        }
+
+        public FastScanner() {
+            this(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String readNextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] readIntArray(int n) {
+            int[] a = new int[n];
+            for (int idx = 0; idx < n; idx++) {
+                a[idx] = nextInt();
+            }
+            return a;
+        }
+    }
+
+}
